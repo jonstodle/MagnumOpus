5bd2de44c2f22d81f7ec8c51fdd11632d5939655
Jon Stødle <jonstodle@outlook.com>
2016-08-22 14:52:37 +0200

    Switch to two way binding

--- SupportTool/MainWindow.xaml.cs
+++ SupportTool/MainWindow.xaml.cs
@@ -56,14 +56,14 @@ namespace SupportTool
 
             this.Bind(ViewModel, vm => vm.UserQueryString, v => v.UserQueryStringTextBox.Text);
             this.OneWayBind(ViewModel, vm => vm.User, v => v.UserDetailsStackPanel.Visibility, x => x != null ? Visibility.Visible : Visibility.Collapsed);
-            this.OneWayBind(ViewModel, vm => vm.User, v => v.UserDetails.User);
-            this.OneWayBind(ViewModel, vm => vm.User, v => v.UserPasswordPanel.User);
-            this.OneWayBind(ViewModel, vm => vm.User, v => v.UserGroups.User);
+            this.Bind(ViewModel, vm => vm.User, v => v.UserDetails.User);
+            this.Bind(ViewModel, vm => vm.User, v => v.UserPasswordPanel.User);
+            this.Bind(ViewModel, vm => vm.User, v => v.UserGroups.User);
 
             this.Bind(ViewModel, vm => vm.ComputerQueryString, v => v.ComputerQueryStringTextBox.Text);
             this.OneWayBind(ViewModel, vm => vm.Computer, v => v.ComputerDetailsStackPanel.Visibility, x => x != null ? Visibility.Visible : Visibility.Collapsed);
-            this.OneWayBind(ViewModel, vm => vm.Computer, v => v.ComputerDetails.Computer);
-            this.OneWayBind(ViewModel, vm => vm.Computer, v => v.PingPanel.Computer);
+            this.Bind(ViewModel, vm => vm.Computer, v => v.ComputerDetails.Computer);
+            this.Bind(ViewModel, vm => vm.Computer, v => v.PingPanel.Computer);
 
 
 
