18f04455f5a96bac3efa17858448400387cae68e
Jon Stødle <jonstodle@outlook.com>
2016-08-26 10:18:36 +0200

    Add DialogService

---
+++ SupportTool/Services/DialogServices/DialogService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SupportTool.Services.DialogServices
+{
+    public class DialogService
+    {
+        public static void ShowMessage(string text, string caption = "", MessageBoxButton button = MessageBoxButton.OK, MessageBoxImage icon = MessageBoxImage.None) => MessageBox.Show(text, caption, button, icon);
+
+        public static void ShowError(string text, string caption = "An error occured") => ShowMessage(text, caption, icon: MessageBoxImage.Error);
+
+        public static void ShowInfo(string text, string caption = "") => ShowMessage(text, caption, icon: MessageBoxImage.Information);
+
+        public static bool ShowPrompt(string question, string caption = "", bool showCancel = false) => MessageBox.Show(question, caption, !showCancel ? MessageBoxButton.YesNo : MessageBoxButton.YesNoCancel, MessageBoxImage.Question) == MessageBoxResult.Yes;
+    }
+}
