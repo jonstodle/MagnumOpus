47779af4ad09cd351bf4f07399d1a9f8c6689803
Jon Stødle <jonstodle@outlook.com>
2016-08-31 14:15:16 +0200

    Change name to Magnum Opus

--- SupportTool/Properties/AssemblyInfo.cs
+++ SupportTool/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SupportTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Magnum Opus by Jon Stødle")]
+[assembly: AssemblyDescription("Lets perform common service desk tasks from one application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sykehuspartner")]
-[assembly: AssemblyProduct("SupportTool")]
+[assembly: AssemblyProduct("Magnum Opus")]
 [assembly: AssemblyCopyright("Copyright © Sykehuspartner 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
