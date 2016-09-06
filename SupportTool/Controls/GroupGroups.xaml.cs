7aec21c1dd61e7ebc9d8a14f24ec2da2f68c6969
Jon Stødle <jonstodle@outlook.com>
2016-09-06 15:04:54 +0200

    Add visuals and make toggle buttons work

---
+++ SupportTool/Controls/GroupGroups.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ReactiveUI;
+using SupportTool.Models;
+using SupportTool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupportTool.Controls
+{
+	/// <summary>
+	/// Interaction logic for GroupGroups.xaml
+	/// </summary>
+	public partial class GroupGroups : UserControl, IViewFor<GroupGroupsViewModel>
+	{
+		public GroupGroups()
+		{
+			InitializeComponent();
+
+			this.Bind(ViewModel, vm => vm.IsShowingDirectMemberOf, v => v.DirectMemberOfToggleButton.IsChecked);
+			this.OneWayBind(ViewModel, vm => vm.IsShowingDirectMemberOf, v => v.DirectMemberOfGrid.Visibility);
+			this.Bind(ViewModel, vm => vm.IsShowingMemberOf, v => v.MemberOfToggleButton.IsChecked);
+			this.OneWayBind(ViewModel, vm => vm.IsShowingMemberOf, v => v.MemberOfGrid.Visibility);
+			this.Bind(ViewModel, vm => vm.IsShowingMembers, v => v.MembersToggleButton.IsChecked);
+			this.OneWayBind(ViewModel, vm => vm.IsShowingMembers, v => v.MembersGrid.Visibility);
+		}
+
+		public GroupObject Group
+		{
+			get { return (GroupObject)GetValue(GroupProperty); }
+			set { SetValue(GroupProperty, value); }
+		}
+
+		public static readonly DependencyProperty GroupProperty = DependencyProperty.Register(nameof(Group), typeof(GroupObject), typeof(GroupGroups), new PropertyMetadata(null, (d,e)=> (d as GroupGroups).ViewModel.Group = e.NewValue as GroupObject));
+
+		public GroupGroupsViewModel ViewModel
+		{
+			get { return (GroupGroupsViewModel)GetValue(ViewModelProperty); }
+			set { SetValue(ViewModelProperty, value); }
+		}
+
+		public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel), typeof(GroupGroupsViewModel), typeof(GroupGroups), new PropertyMetadata(new GroupGroupsViewModel()));
+
+		object IViewFor.ViewModel
+		{
+			get { return ViewModel; }
+			set { ViewModel = value as GroupGroupsViewModel; }
+		}
+	}
+}
---
+++ SupportTool/ViewModels/GroupGroupsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using ReactiveUI;
+using SupportTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportTool.ViewModels
+{
+	public class GroupGroupsViewModel : ReactiveObject
+	{
+		private GroupObject _group;
+		private bool _isShowingDirectMemberOf;
+		private bool _isShowingMemberOf;
+		private bool _isShowingMembers;
+
+
+
+		public GroupGroupsViewModel()
+		{
+			this
+				.WhenAnyValue(x => x.IsShowingMemberOf, y => y.IsShowingMembers, (x, y) => x || y)
+				.Where(x => x)
+				.Subscribe(_ => IsShowingDirectMemberOf = false);
+			this
+				.WhenAnyValue(x => x.IsShowingDirectMemberOf, y => y.IsShowingMembers, (x, y) => x || y)
+				.Where(x => x)
+				.Subscribe(_ => IsShowingMemberOf = false);
+			this
+				.WhenAnyValue(x => x.IsShowingDirectMemberOf, y => y.IsShowingMemberOf, (x, y) => x || y)
+				.Where(x => x)
+				.Subscribe(_ => IsShowingMembers = false);
+		}
+
+
+
+		public GroupObject Group
+		{
+			get { return _group; }
+			set { this.RaiseAndSetIfChanged(ref _group, value); }
+		}
+
+		public bool IsShowingDirectMemberOf
+		{
+			get { return _isShowingDirectMemberOf; }
+			set { this.RaiseAndSetIfChanged(ref _isShowingDirectMemberOf, value); }
+		}
+
+		public bool IsShowingMemberOf
+		{
+			get { return _isShowingMemberOf; }
+			set { this.RaiseAndSetIfChanged(ref _isShowingMemberOf, value); }
+		}
+
+		public bool IsShowingMembers
+		{
+			get { return _isShowingMembers; }
+			set { this.RaiseAndSetIfChanged(ref _isShowingMembers, value); }
+		}
+	}
+}
