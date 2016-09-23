a4115f3b5dd8ff0602b1101705180f8d4315fc2d
Jon Stødle <jonstodle@outlook.com>
2016-09-23 12:47:46 +0200

    Switch to Name

--- SupportTool/Controls/UserDetails.xaml.cs
+++ SupportTool/Controls/UserDetails.xaml.cs
@@ -18,7 +18,7 @@ namespace SupportTool.Controls
 
 			ViewModel = new UserDetailsViewModel();
 
-            this.OneWayBind(ViewModel, vm => vm.User.Principal.DisplayName, v => v.DisplayNameTextBlock.Text);
+            this.OneWayBind(ViewModel, vm => vm.User.Principal.Name, v => v.DisplayNameTextBlock.Text);
 			this.OneWayBind(ViewModel, vm => vm.User.Principal.SamAccountName, v => v.SamTextBlock.Text, x => x?.ToUpperInvariant());
 			this.OneWayBind(ViewModel, vm => vm.User.Company, v => v.CompanyTextBlock.Text, x => x.HasValue() ? x : "No company");
             this.OneWayBind(ViewModel, vm => vm.User.Principal.AccountExpirationDate, v => v.ExpirationTextBlock.Text, x => x != null ? $"Expires {((DateTime)x).ToShortDateString()}" : "Never expires");
