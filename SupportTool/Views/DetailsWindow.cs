b2c3c0d66c83bb3835d6122c0d37df6e3542c211
Jon Stødle <jonstodle@outlook.com>
2016-10-28 14:21:06 +0200

    Change implementation of details window

---
+++ SupportTool/Views/DetailsWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SupportTool.Views
+{
+	public class DetailsWindow : Window
+	{
+		private CompositeDisposable _subscriptions = new CompositeDisposable();
+
+		public DetailsWindow()
+		{
+			Observable.Interval(TimeSpan.FromHours(2), Scheduler.TaskPool)
+				.ObserveOnDispatcher()
+				.Subscribe(x => this.Close())
+				.AddTo(_subscriptions);
+
+			this.Events().Closed
+				.Subscribe(x => _subscriptions.Dispose())
+				.AddTo(_subscriptions);
+		}
+	}
+}
