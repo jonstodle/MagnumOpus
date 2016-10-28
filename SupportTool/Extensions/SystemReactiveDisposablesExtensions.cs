454e8c20e3cd676b3fe26eeba8b9515092583fe8
Jon Stødle <jonstodle@outlook.com>
2016-10-28 14:12:36 +0200

    Add base window which closes automatically

---
+++ SupportTool/Extensions/SystemReactiveDisposablesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Reactive.Disposables
+{
+	public static class SystemReactiveDisposablesExtensions
+	{
+		public static void AddTo(this IDisposable source, CompositeDisposable cd) => cd.Add(source);
+	}
+}
