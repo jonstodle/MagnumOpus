b6c961eb08edddb678a3eef8eb9e07eba9ff7d83
Jon Stødle <jonstodle@outlook.com>
2016-11-11 15:45:52 +0100

    Make message info generic

--- SupportTool/Models/MessageInfo.cs
+++ SupportTool/Models/MessageInfo.cs
@@ -8,25 +8,17 @@ namespace SupportTool.Models
 {
 	public struct MessageInfo
 	{
-		public MessageType MessageType { get; private set; }
 		public string Caption { get; private set; }
 		public string Message { get; private set; }
 
-		public MessageInfo(MessageType type, string message, string caption = "")
+		public MessageInfo(string message, string caption = "")
 		{
-			MessageType = type;
 			Message = message;
 			Caption = caption;
 		}
 
-		public static MessageInfo PasswordSetMessageInfo(string password) => new MessageInfo(MessageType.Info, "Password set", $"New password is: {password}\nMust be changed at next logon");
+		public static MessageInfo PasswordSetMessageInfo(string password) => new MessageInfo("Password set", $"New password is: {password}\nMust be changed at next logon");
 
-		public static MessageInfo PasswordSetErrorMessageInfo(string message = null) => new MessageInfo(MessageType.Error, "Password not set", message ?? $"Could not set password");
-	}
-
-	public enum MessageType
-	{
-		Info,
-		Error
+		public static MessageInfo PasswordSetErrorMessageInfo(string message = null) => new MessageInfo("Password not set", message ?? $"Could not set password");
 	}
 }
