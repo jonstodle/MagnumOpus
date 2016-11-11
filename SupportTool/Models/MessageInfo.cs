d8ab367d1c96a48accf625adbda51151f9721239
Jon Stødle <jonstodle@outlook.com>
2016-11-11 15:44:05 +0100

    Add MessageInfo

---
+++ SupportTool/Models/MessageInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportTool.Models
+{
+	public struct MessageInfo
+	{
+		public MessageType MessageType { get; private set; }
+		public string Caption { get; private set; }
+		public string Message { get; private set; }
+
+		public MessageInfo(MessageType type, string message, string caption = "")
+		{
+			MessageType = type;
+			Message = message;
+			Caption = caption;
+		}
+
+		public static MessageInfo PasswordSetMessageInfo(string password) => new MessageInfo(MessageType.Info, "Password set", $"New password is: {password}\nMust be changed at next logon");
+
+		public static MessageInfo PasswordSetErrorMessageInfo(string message = null) => new MessageInfo(MessageType.Error, "Password not set", message ?? $"Could not set password");
+	}
+
+	public enum MessageType
+	{
+		Info,
+		Error
+	}
+}
