d6541dbdbd7dc8d50bad3b1c8f6531eb6d80654f
Jon Stødle <jonstodle@outlook.com>
2016-11-11 11:29:56 +0100

    Basic implemenation

---
+++ SupportTool/Controls/DialogControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupportTool.Controls
+{
+	/// <summary>
+	/// Interaction logic for DialogControl.xaml
+	/// </summary>
+	public partial class DialogControl : UserControl
+	{
+		public IObservable<int> Result => _resultSubject;
+
+		private DialogControl()
+		{
+			InitializeComponent();
+		}
+
+		public DialogControl(Grid parent, string text, params string[] buttonTitles)
+		{
+			InitializeComponent();
+
+			SetValue(Grid.RowSpanProperty, parent.RowDefinitions.Count > 0 ? parent.RowDefinitions.Count : 1);
+			SetValue(Grid.ColumnSpanProperty, parent.ColumnDefinitions.Count > 0 ? parent.ColumnDefinitions.Count : 1);
+
+			MessageTextBlock.Text = text;
+
+			_buttonTitles = buttonTitles;
+			foreach (var buttonTitle in _buttonTitles)
+			{
+				var button = new Button { Content = buttonTitle };
+				button.Click += HandleButtonClick;
+				ButtonStackPanel.Children.Add(button);
+			}
+
+			parent.Children.Add(this);
+		}
+
+
+
+		private void HandleButtonClick(object sender, RoutedEventArgs args)
+		{
+			if (_resultSubject == null) return;
+
+			var content = (sender as Button).Content as string;
+			if (content == null) return;
+
+			_resultSubject.OnNext(Array.IndexOf(_buttonTitles, content));
+			_resultSubject.OnCompleted();
+			_resultSubject = null;
+			
+			//TODO: Close dialog
+		}
+
+
+
+		private ISubject<int> _resultSubject = new Subject<int>();
+		private string[] _buttonTitles;
+	}
+}
