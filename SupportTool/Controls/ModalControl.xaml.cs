2bab6c8f77c1dbf719961ad10751f27d5caf0be9
Jon Stødle <jonstodle@outlook.com>
2016-11-16 12:57:32 +0100

    Base implementation

---
+++ SupportTool/Controls/ModalControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupportTool.Controls
+{
+	/// <summary>
+	/// Interaction logic for ModalControl.xaml
+	/// </summary>
+	public partial class ModalControl : UserControl
+	{
+		public ModalControl(object parameters = null)
+		{
+			InitializeComponent();
+
+			_parameters = parameters;
+		}
+
+
+
+		public object DisplayContent
+		{
+			get { return (object)GetValue(DisplayContentProperty); }
+			set { SetValue(DisplayContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty DisplayContentProperty = DependencyProperty.Register(nameof(DisplayContent), typeof(object), typeof(ModalControl), new PropertyMetadata(null, (d, e) => (d as ModalControl).PassParameters()));
+
+
+
+		private void PassParameters()
+		{
+
+		}
+
+
+
+		object _parameters;
+	}
+}
