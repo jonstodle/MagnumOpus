a98cacd7299eca024d6a12844df8c76ddddd6e28
Jon Stødle <jonstodle@outlook.com>
2016-11-17 13:53:46 +0100

    Change to passing control instance

--- SupportTool/Models/DialogInfo.cs
+++ SupportTool/Models/DialogInfo.cs
@@ -12,10 +12,10 @@ namespace SupportTool.Models
 {
 	public struct DialogInfo
 	{
-		public Type Control { get; private set; }
+		public Control Control { get; private set; }
 		public object Parameter { get; private set; }
 
-		public DialogInfo(Type control, object parameter)
+		public DialogInfo(Control control, object parameter)
 		{
 			Control = control;
 			Parameter = parameter;
@@ -26,6 +26,10 @@ namespace SupportTool.Models
 
 	public static class DialogInfoHelpers
 	{
-		public static IDisposable RegisterDialogHandler(this Interaction<DialogInfo, Unit> source, Grid parent) => source.RegisterHandler(interaction => { new ModalControl(parent, Activator.CreateInstance(interaction.Input.Control), interaction.Input.Parameter); });
+		public static IDisposable RegisterDialogHandler(this Interaction<DialogInfo, Unit> source, Grid parent) => source.RegisterHandler(interaction => 
+		{
+			new ModalControl(parent, interaction.Input.Control, interaction.Input.Parameter);
+			interaction.SetOutput(Unit.Default);
+		});
 	}
 }
