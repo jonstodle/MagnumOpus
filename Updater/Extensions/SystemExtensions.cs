2b4aeef13a5ab0a437d3025f01593906807b3022
Jon Stødle <jonstodle@outlook.com>
2016-11-24 14:01:31 +0100

    Add string extension

---
+++ Updater/Extensions/SystemExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public static class SystemExtensions
+	{
+		public static bool HasValue(this string source, int minimumLength = 0) => string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
+	}
+}
