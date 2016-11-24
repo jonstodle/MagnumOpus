8d48b87dc17c088415d6030d7aa43b6f9429978c
Jon Stødle <jonstodle@outlook.com>
2016-11-24 14:24:00 +0100

    Fix not reversing bool value

--- Updater/Extensions/SystemExtensions.cs
+++ Updater/Extensions/SystemExtensions.cs
@@ -8,6 +8,6 @@ namespace System
 {
 	public static class SystemExtensions
 	{
-		public static bool HasValue(this string source, int minimumLength = 0) => string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
+		public static bool HasValue(this string source, int minimumLength = 0) => !string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
 	}
 }
