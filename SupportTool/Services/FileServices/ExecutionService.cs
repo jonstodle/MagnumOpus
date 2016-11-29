18c271ba0f870567f5fee4ee05fbfe731b3107f6
Jon Stødle <jonstodle@outlook.com>
2016-11-29 12:39:25 +0100

    Add helper for executing bundled exes

--- SupportTool/Services/FileServices/ExecutionService.cs
+++ SupportTool/Services/FileServices/ExecutionService.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 
+using static SupportTool.Executables.Helpers;
+
 namespace SupportTool.Services.FileServices
 {
 	public static class ExecutionService
@@ -14,6 +16,13 @@ namespace SupportTool.Services.FileServices
 		{
 			if (File.Exists(fileName)) Process.Start(fileName, arguments);
 			else throw new ArgumentException($"Could not find {fileName}");
+		}
+
+		public static void ExecuteInternalFile(string fileName, string arguments = "")
+		{
+			var filePath = Path.Combine(FileService.LocalAppData, fileName);
+			EnsureExecutableIsAvailable(fileName);
+			ExecuteFile(filePath, arguments);
 		}
 	}
 }
