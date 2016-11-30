a62a63247a756ea2d81f323732340847339e248d
Jon Stødle <jonstodle@outlook.com>
2016-11-30 07:27:27 +0100

    Add error handling

--- Updater/ViewModels/MainWindowViewModel.cs
+++ Updater/ViewModels/MainWindowViewModel.cs
@@ -41,8 +41,13 @@ namespace Updater.ViewModels
 					this.WhenAnyValue(x => x.SourceFilePath, x => x.HasValue(1)),
 					_destinationFolders.CountChanged.Select(x => x > 0),
 					(sourceFilePath, destinationFolders) => sourceFilePath && destinationFolders));
-			_confirm
-				.ThrownExceptions
+
+			Observable.Merge(
+				_browseForSourceFile.ThrownExceptions,
+				_browseForDestinationFolder.ThrownExceptions,
+				_addDestinationFolder.ThrownExceptions,
+				_removeDestinationFolder.ThrownExceptions,
+				_confirm.ThrownExceptions)
 				.Subscribe(ex => MessageBox.Show(ex.Message, "An error occured", MessageBoxButtons.OK, MessageBoxIcon.Error));
 
 			_destinationFoldersSortedView = _destinationFolders.CreateDerivedCollection(x => x, orderer: (x, y) => x.CompareTo(y));
