c0cbf1a1710b9fbb0adef702249d64711d8571fe
Jon Stødle <jonstodle@outlook.com>
2016-12-02 12:51:52 +0100

    Add support for hiding started process' window

--- SupportTool/Services/FileServices/ExecutionService.cs
+++ SupportTool/Services/FileServices/ExecutionService.cs
@@ -10,21 +10,21 @@ namespace SupportTool.Services.FileServices
 	{
 		public static string System32Path => Environment.GetFolderPath(Environment.SpecialFolder.System);
 
-		public static void ExecuteCmd(string fileName, string arguments = "") => ExecuteFile(Path.Combine(System32Path, "cmd.exe"), $@"/K {fileName} {arguments}");
+		public static void ExecuteCmd(string fileName, string arguments = "", bool showWindow = true) => ExecuteFile(Path.Combine(System32Path, "cmd.exe"), $@"/K {fileName} {arguments}", showWindow);
 
-		public static void ExecuteFile(string fileName, string arguments = "")
+		public static void ExecuteFile(string fileName, string arguments = "", bool showWindow = true)
 		{
-			if (File.Exists(fileName)) Process.Start(fileName, arguments);
+			if (File.Exists(fileName)) Process.Start(new ProcessStartInfo(fileName, arguments) { CreateNoWindow = !showWindow });
 			else throw new ArgumentException($"Could not find {fileName}");
 		}
 
-		public static void ExecuteInternalCmd(string fileName, string arguments = "") => ExecuteFile(Path.Combine(System32Path, "cmd.exe"), $"/K \"{Path.Combine(FileService.LocalAppData, fileName)}\" {arguments}");
+		public static void ExecuteInternalCmd(string fileName, string arguments = "", bool showWindow = true) => ExecuteFile(Path.Combine(System32Path, "cmd.exe"), $"/K \"{Path.Combine(FileService.LocalAppData, fileName)}\" {arguments}", showWindow);
 
-		public static void ExecuteInternalFile(string fileName, string arguments = "")
+		public static void ExecuteInternalFile(string fileName, string arguments = "", bool showWindow = true)
 		{
 			var filePath = Path.Combine(FileService.LocalAppData, fileName);
 			EnsureExecutableIsAvailable(fileName);
-			ExecuteFile(filePath, arguments);
+			ExecuteFile(filePath, arguments, showWindow);
 		}
 	}
 }
