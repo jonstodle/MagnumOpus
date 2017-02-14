fb0c60ef8b6b989e9e12abd1774598ad5fc7f4e2
Jon Stødle <jonstodle@outlook.com>
2017-02-14 12:12:25 +0100

    Show Company instead of ADS path in search results

--- MagnumOpus/Models/DirectoryEntryInfo.cs
+++ MagnumOpus/Models/DirectoryEntryInfo.cs
@@ -13,6 +13,7 @@ namespace MagnumOpus.Models
 			DistinguishedName = directoryEntry.Properties["distinguishedname"].Value?.ToString();
 			SamAccountName = directoryEntry.Properties["samaccountname"].Value?.ToString();
 			Name = directoryEntry.Properties["name"].Value?.ToString();
+            Company = directoryEntry.Properties.Get<string>("company");
 			ObjectType = directoryEntry.Properties["objectcategory"].Value?.ToString().Split(',').FirstOrDefault()?.Split('=').Skip(1).FirstOrDefault();
 
 			directoryEntry.Dispose();
@@ -20,12 +21,13 @@ namespace MagnumOpus.Models
 
 
 
-        public string Path { get; set; }
-        public string CN { get; set; }
-        public string DistinguishedName { get; set; }
-        public string SamAccountName { get; set; }
-        public string Name { get; set; }
-        public string ObjectType { get; set; }
+        public string Path { get; private set; }
+        public string CN { get; private set; }
+        public string DistinguishedName { get; private set; }
+        public string SamAccountName { get; private set; }
+        public string Name { get; private set; }
+        public string Company { get; private set; }
+        public string ObjectType { get; private set; }
 
 
 
