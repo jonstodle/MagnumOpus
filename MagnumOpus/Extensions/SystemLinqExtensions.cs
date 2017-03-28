bbe0c1f531c4a87473ac300a90307cc89e8ef2b6
Jon Stødle <jonstodle@outlook.com>
2017-03-28 12:56:38 +0200

    Add extension method to convert IEnumerable to IEnumerable<T>

---
+++ MagnumOpus/Extensions/SystemLinqExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static class SystemLinqExtensions
+    {
+        public static IEnumerable<T> ToGeneric<T>(this IEnumerable source) { foreach (var item in source) yield return (T)item; }
+    }
+}
