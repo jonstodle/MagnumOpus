4772672f09027fdaace2e5e388546f6e3816e688
Jon Stødle <jonstodle@outlook.com>
2017-04-12 11:39:18 +0200

    Add documentation

--- MagnumOpus/Services/FileServices/ExecutionService.cs
+++ MagnumOpus/Services/FileServices/ExecutionService.cs
@@ -8,12 +8,27 @@ namespace MagnumOpus.Services.FileServices
 {
 	public static class ExecutionService
 	{
+        /// <summary>
+        /// Path to the System32 folder on the system
+        /// </summary>
 		public static string System32Path => Environment.GetFolderPath(Environment.SpecialFolder.System);
 
 
 
+        /// <summary>
+        /// Runs a file in CMD with the given arguments
+        /// </summary>
+        /// <param name="filePath">Full path to the file to be executed</param>
+        /// <param name="arguments">Arguments to be passed</param>
+        /// <param name="showWindow">Set to false to hide CMD window when executing the file</param>
         public static void RunInCmd(string filePath, string arguments = "", bool showWindow = true) => RunFile(Path.Combine(System32Path, "cmd.exe"), $@"/K {filePath} {arguments}", showWindow);
 
+        /// <summary>
+        /// Runs a file in CMD with the given arguments and returns the output
+        /// </summary>
+        /// <param name="filePath">Full path to the file to be executed</param>
+        /// <param name="arguments">Arguments to be passed</param>
+        /// <returns>A string containing the output of the executed file</returns>
         public static string RunInCmdWithOuput(string filePath, string arguments = "")
         {
             if (!File.Exists(filePath)) throw new ArgumentException($"Could not find {filePath}");
@@ -30,6 +45,12 @@ namespace MagnumOpus.Services.FileServices
             .ReadToEnd();
         }
 
+        /// <summary>
+        /// Runs a file in a new process with the given arguments
+        /// </summary>
+        /// <param name="filePath">Full path to the file to be executed</param>
+        /// <param name="arguments">Arguments to be passed</param>
+        /// <param name="showWindow">Set to false to hide window</param>
         public static void RunFile(string filePath, string arguments = "", bool showWindow = true)
 		{
 			if (File.Exists(filePath)) Process.Start(new ProcessStartInfo(filePath, arguments) { CreateNoWindow = !showWindow });
@@ -38,8 +59,20 @@ namespace MagnumOpus.Services.FileServices
 
 
 
+        /// <summary>
+        /// Runs a file bundled in the application package in CMD with the given arguments
+        /// </summary>
+        /// <param name="fileName">Name of the file</param>
+        /// <param name="arguments">Arguments to be passed</param>
+        /// <param name="showWindow">Set to false to hide window</param>
         public static void RunInCmdFromCache(string fileName, string arguments = "", bool showWindow = true) => RunFile(Path.Combine(System32Path, "cmd.exe"), $"/K \"{Path.Combine(FileService.LocalAppData, fileName)}\" {arguments}", showWindow);
 
+        /// <summary>
+        /// Runs a file bundled in the application package in a new process with the given arguments
+        /// </summary>
+        /// <param name="fileName">Name of the file</param>
+        /// <param name="arguments">Arguments to be passed</param>
+        /// <param name="showWindow">Set to false to hide window</param>
 		public static void RunFileFromCache(string fileName, string arguments = "", bool showWindow = true)
 		{
 			var filePath = Path.Combine(FileService.LocalAppData, fileName);
