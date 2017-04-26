f17cc1ae4843ddb37a5ee6b417148011376d7849
Jon Stødle <jonstodle@outlook.com>
2017-04-26 10:24:32 +0200

    Hide logoff command

--- MagnumOpus/Controls/RemotePanel.xaml.cs
+++ MagnumOpus/Controls/RemotePanel.xaml.cs
@@ -35,7 +35,7 @@ namespace MagnumOpus.Controls
                 d(_loggedOnUsersListViewItemDoubleClick.ToEventCommandSignal().InvokeCommand(ViewModel.OpenUser));
                 d(this.BindCommand(ViewModel, vm => vm.OpenUser, v => v.OpenUserMenuItem));
                 d(this.BindCommand(ViewModel, vm => vm.CopyUserName, v => v.CopyUsernameMenuItem));
-                d(this.BindCommand(ViewModel, vm => vm.LogOffUser, v => v.LogOffUserMenuItem));
+                //d(this.BindCommand(ViewModel, vm => vm.LogOffUser, v => v.LogOffUserMenuItem));
                 d(this.BindCommand(ViewModel, vm => vm.StartRemoteControl, v => v.RemoteControlButton));
                 d(this.BindCommand(ViewModel, vm => vm.StartRemoteControlClassic, v => v.RemoteControlClassicButton));
                 d(this.BindCommand(ViewModel, vm => vm.StartRemoteControl2012, v => v.RemoteControl2012Button));
