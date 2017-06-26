c25e184b8274bc452648dcdbe0d8d6062702a5e4
Jon Stødle <jonstodle@outlook.com>
2017-06-26 13:37:44 +0200

    Add easier access to ResultPropertyCollections

--- MagnumOpus/Extensions/SystemDirectoryServicesExtensions.cs
+++ MagnumOpus/Extensions/SystemDirectoryServicesExtensions.cs
@@ -16,7 +16,19 @@ namespace System.DirectoryServices
 			return (T)propCollection[index];
 		}
 
-		public static IEnumerable<T> ToEnumerable<T>(this PropertyValueCollection pvc)
+        public static T Get<T>(this ResultPropertyCollection rpc, string propertyName, int index = 0)
+        {
+            var propCollection = rpc[propertyName];
+
+            if (propCollection.Count == 0
+                || index >= propCollection.Count
+                || index < 0)
+            { return default(T); }
+
+            return (T)propCollection[index];
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this PropertyValueCollection pvc)
 		{
 			var result = new List<T>();
 			foreach (T val in pvc) result.Add(val);
